4afdaa204c8f9407750e46050d88fa82c04105cc
Yavuz Taha Tulumbacı <yavuztaha026@gmail.com>
2026-05-30 23:52:26 +0300

    Database bağlantı şekli değiştirildi.

--- config/Database.php
+++ config/Database.php
@@ -2,31 +2,55 @@
 
 class Database
 {
-    private $host = "127.0.0.1";
-    private $dbname = "catering_db";
-    private $username = "root";
-    private $password = "";
-    private $charset = "utf8mb4";
+    private static ?PDO $instance = null;
 
-    private ?PDO $pdo;
+    private function __construct() {}
+    private function __clone() {}
 
 
-    public function getConnection()
+    public static function getConnection(): PDO
     {
-        $this->pdo = null;
-
-        try {
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
+        if (self::$instance == null) {
+            $dsn = "mysql:host=127.1.1.0;dbname=catering_db;charset=utf8mb4";
             $options = [
-
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false
+                PDO::ATTR_EMULATE_PREPARES   => false,
             ];
-            $this->pdo = new PDO($dsn, $this->username, $this->password, $options);
-        } catch (\PDOException $e) {
-            die("Veritabanına bağlanılamadı: " . $e->getMessage());
+            try {
+                self::$instance = new PDO($dsn, 'root', '', $options);
+            } catch (\PDOException $e) {
+                die("Veri tabanına bağlanılamadı: " . $e->getMessage());
+            }
         }
-        return $this->pdo;
+        return self::$instance;
     }
+
+    // private $host = "127.0.0.1";
+    // private $dbname = "catering_db";
+    // private $username = "root";
+    // private $password = "";
+    // private $charset = "utf8mb4";
+
+    // private ?PDO $pdo;
+
+
+    // public function getConnection()
+    // {
+    //     $this->pdo = null;
+
+    //     try {
+    //         $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
+    //         $options = [
+
+    //             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    //             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //             PDO::ATTR_EMULATE_PREPARES => false
+    //         ];
+    //         $this->pdo = new PDO($dsn, $this->username, $this->password, $options);
+    //     } catch (\PDOException $e) {
+    //         die("Veritabanına bağlanılamadı: " . $e->getMessage());
+    //     }
+    //     return $this->pdo;
+    // }
 }
